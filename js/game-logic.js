56f6dd94a1d696793d596f5d740a0d3039f6b6aa
John.michael <John.michael@studentambassadors.com>
2021-12-01 20:16:43 -0600

    added all global variables for test

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,2 +1,16 @@
 // All code should be written in this file.
-let playerOneMoveType;
+let playerOneMoveOneType,
+playerOneMoveTwoType,
+playerOneMoveThreeType,
+playerTwoMoveOneType,
+playerTwoMoveTwoType,
+playerTwoMoveThreeType,
+playerOneMoveOneValue,
+playerOneMoveTwoValue,
+playerOneMoveThreeValue,
+playerTwoMoveOneValue,
+playerTwoMoveTwoValue,
+playerTwoMoveThreeValue;
+
+//create player moves function pass in to this function player, moveone type,moveone value,movetwotype,movetwovalue,
+function setPlayerMoves(player,playerOneMoveOneType,playerOneMoveOneValue,pla,playerOneMoveThreeType,)
